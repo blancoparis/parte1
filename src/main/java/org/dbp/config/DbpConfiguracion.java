ad2add7bebded74d7fd8be013cb270554eb79de1
david <david@MSI>
2014-05-30 00:16:33 +0200

    Configuracion basica de spring

---
+++ src/main/java/org/dbp/config/DbpConfiguracion.java
@@ -0,0 +1,27 @@
+package org.dbp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class DbpConfiguracion extends WebMvcConfigurerAdapter{
+
+	/**
+	 * 
+	 * Configurar el view resolver
+	 * 
+	 * @return
+	 */
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+	
+}
