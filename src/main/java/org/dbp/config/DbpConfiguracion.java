0c5a6302741d517eeb65bdf4277b4fb3c01ae5c9
david <david@MSI>
2014-06-01 23:35:28 +0200

    Creación de la pagina de inicio.

--- src/main/java/org/dbp/config/DbpConfiguracion.java
+++ src/main/java/org/dbp/config/DbpConfiguracion.java
@@ -1,25 +1,30 @@
 package org.dbp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@ComponentScan({
+	"org.dbp.controlador"
+})
 @EnableWebMvc
 public class DbpConfiguracion extends WebMvcConfigurerAdapter{
 
 	/**
 	 * 
-	 * Configurar el view resolver
+	 * Configurar el view resolver: Las paginas se encontraran apartir del directorio,
+	 * '/WEB-INF/paginas/'+paginas+'.jsp'
 	 * 
 	 * @return
 	 */
     @Bean
     public InternalResourceViewResolver getInternalResourceViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setPrefix("/WEB-INF/paginas/");
         resolver.setSuffix(".jsp");
         return resolver;
     }
